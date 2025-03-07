21c9388564cf700dfb9652287e1248737714bd8f
pablo-alves92 <pablo9225@gmail.com>
2025-03-07 14:00:46 -0300

    commit inicial.

---
+++ xadrez.c
@@ -0,0 +1,8 @@
+/*
+ * Projeto: Xadez em C.
+ * Autor: Pablo Muriell Batista Alves.
+ * Data de criação: 07/03/2025.
+ * Última atualização: 07/03/2025.
+ * Versão: 1.0
+ * Descrição: Implementação do jogo de Xadez em linguagem C, desenvolvido para um projeto acadêmico.
+ */
